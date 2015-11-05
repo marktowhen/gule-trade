e555b6f1b8eef409f7634cd0719fec16093175d9
dxf <dxf@dxf-PC>
2015-11-05 10:39:15 +0800

    商品查询

--- src/main/java/com/jingyunbank/etrade/goods/service/GoodsService.java
+++ src/main/java/com/jingyunbank/etrade/goods/service/GoodsService.java
@@ -1,25 +1,60 @@
 package com.jingyunbank.etrade.goods.service;
 
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.jingyunbank.core.util.CollectionUtils;
 import com.jingyunbank.etrade.api.goods.bo.Goods;
 import com.jingyunbank.etrade.api.goods.service.IGoodsService;
+import com.jingyunbank.etrade.goods.dao.GoodsDao;
+import com.jingyunbank.etrade.goods.entity.GoodsDaoVO;
+
 /**
  * 
-* Title: 商品接口
-* @author    duanxf
-* @date      2015年11月4日
+ * Title: 商品接口
+ * 
+ * @author duanxf
+ * @date 2015年11月4日
  */
 @Service("goodsService")
-public class GoodsService implements IGoodsService{
+public class GoodsService implements IGoodsService {
+	@Resource
+	private GoodsDao goodsDao;
 
-	public List<Goods> queryByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	@Override
+	public List<Goods> listGoodsByLikeName(String goodsname) throws Exception {
+		List<GoodsDaoVO> daolist = goodsDao.selectGoodsByLikeName(goodsname);
+		List<Goods> bolist = new ArrayList<Goods>();
+		if (daolist != null) {
+				bolist = CollectionUtils.copyTo(daolist, Goods.class);
+		}
+		return bolist;
 	}
 
+	@Override
+	public List<Goods> listBrands() throws Exception {
+		List<Goods> brandslist = new ArrayList<Goods>();
+		List<GoodsDaoVO>  Brands = goodsDao.selectBrands();
+		if(Brands!=null) {
+			brandslist = CollectionUtils.copyTo(Brands, Goods.class);
+		}
+		return brandslist;
+	}
 
+	@Override
+	public List<Goods> listTypes() throws Exception {
+		List<Goods> typeslist = new ArrayList<Goods>();
+		List<GoodsDaoVO>  types = goodsDao.selectTypes();
+		if(types!=null) {
+			typeslist = CollectionUtils.copyTo(types, Goods.class);
+		}
+		return typeslist;
+	}
 
 }
