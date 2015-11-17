8b78aca2f8a962272ba0b4e158d05d18c0ca8d25
Mike Cao <michael-cao@outlook.com>
2015-11-17 09:12:36 +0800

    upgrade spring boot from 1.2.6 to 1.3.0

--- src/main/java/com/jingyunbank/etrade/config/MvcConfig.java
+++ src/main/java/com/jingyunbank/etrade/config/MvcConfig.java
@@ -1,5 +1,6 @@
 package com.jingyunbank.etrade.config;
 
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import com.jingyunbank.etrade.api.base.intercepter.AuthIntercepter;
 
 @Configuration
+@ServletComponentScan(basePackages="com.jingyunbank.etrade")
 public class MvcConfig {
 	
 	@Bean
