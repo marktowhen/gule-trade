fe4a1a050839f93d3d3a676af443f8b60fc5ce32
Mike Cao <michael-cao@outlook.com>
2015-11-28 14:31:06 +0800

    pay update.

--- src/main/java/com/jingyunbank/etrade/pay/handler/jdpay/JDPayHandler.java
+++ src/main/java/com/jingyunbank/etrade/pay/handler/jdpay/JDPayHandler.java
@@ -1,6 +1,5 @@
 package com.jingyunbank.etrade.pay.handler.jdpay;
 
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +14,7 @@ public class JDPayHandler implements PayHandler {
 	public Map<String, String> handle(List<OrderPayment> payments)
 			throws Exception {
 		Map<String, String> result = new HashMap<String, String>();
-		String vamount = payments.stream().map(x->x.getMoney()).reduce(new BigDecimal(0), (a, b)->a.add(b)).toString();
+		String vamount = "0.01";//payments.stream().map(x->x.getMoney()).reduce(new BigDecimal(0), (a, b)->a.add(b)).toString();
 		String v_moneytype = "CNY";
 		String v_oid = String.valueOf(payments.get(0).getExtransno());
 		String v_mid = "1001";
@@ -27,7 +26,7 @@ public class JDPayHandler implements PayHandler {
 		result.put("v_amount", vamount);
 		result.put("v_moneytype", v_moneytype);
 		result.put("v_url", v_url);
-		result.put("pmode_id", "3080");
+		result.put("pmode_id", "1025");
 		result.put("v_md5info", MD5.digest(vamount+v_moneytype+v_oid+v_mid+v_url+key));
 		result.put("remark2", v_url);
 		
