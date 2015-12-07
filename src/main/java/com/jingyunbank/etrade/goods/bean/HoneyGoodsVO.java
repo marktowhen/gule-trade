aeefe27b5cb8ad69863c2973d222000778b776bb
liuguang <liuguang@jingyunbank.com>
2015-12-07 17:35:29 +0800

    liug_20151207

--- src/main/java/com/jingyunbank/etrade/goods/bean/HoneyGoodsVO.java
+++ src/main/java/com/jingyunbank/etrade/goods/bean/HoneyGoodsVO.java
@@ -11,5 +11,12 @@ public class HoneyGoodsVO extends CommonGoodsVO{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String volume; // 销量
+	public String getVolume() {
+		return volume;
+	}
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
 	
 }
