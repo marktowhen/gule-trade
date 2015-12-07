a0cf308db1fa106a0e35eaa722763dd7e184d65e
liuguang <liuguang@jingyunbank.com>
2015-12-07 14:44:56 +0800

    liug_20151207

---
+++ src/main/java/com/jingyunbank/etrade/goods/bean/HoneyGoodsVO.java
@@ -0,0 +1,15 @@
+package com.jingyunbank.etrade.goods.bean;
+
+/**
+ * 阿胶详情页面宝贝推荐
+ * @author liug
+ *
+ */
+public class HoneyGoodsVO extends CommonGoodsVO{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
