fe07bcc281ed64e8d4313a940e31832fe92ce5b7
liuguang <liuguang@jingyunbank.com>
2015-12-11 17:26:29 +0800

    liug_20151211

--- src/main/java/com/jingyunbank/etrade/back/goods/bean/GoodsListShowVO.java
+++ src/main/java/com/jingyunbank/etrade/back/goods/bean/GoodsListShowVO.java
@@ -1,11 +1,13 @@
 package com.jingyunbank.etrade.back.goods.bean;
 
+import com.jingyunbank.etrade.api.goods.bo.BaseGoods;
+
 /**
  * 商品查询列表对象
  * @author liug
  *
  */
-public class GoodsListShowVO extends CommonGoodsBackVO{
+public class GoodsListShowVO extends BaseGoods{
 
 	/**
 	 * 
