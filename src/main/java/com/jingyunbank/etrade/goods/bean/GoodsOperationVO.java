825bf9dd6351501a824b677837fd3f99b9f40ea3
dxf <duanxiaofeng@jingyunbank.com>
2015-12-15 17:01:32 +0800

    update

--- src/main/java/com/jingyunbank/etrade/goods/bean/GoodsOperationVO.java
+++ src/main/java/com/jingyunbank/etrade/goods/bean/GoodsOperationVO.java
@@ -2,12 +2,10 @@ package com.jingyunbank.etrade.goods.bean;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Date;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
  * 
