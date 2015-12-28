3d4f9328962ac989a63fc62b17950a97f367e160
QiuXingSheng <qiuxingsheng@jingyunbank.com>
2015-12-28 12:04:23 +0800

    test

--- src/test/java/com/jingyunbank/etrade/area/controller/ProvinceControllerTest.java
+++ src/test/java/com/jingyunbank/etrade/area/controller/ProvinceControllerTest.java
@@ -1,11 +1,6 @@
 package com.jingyunbank.etrade.area.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -20,65 +15,7 @@ import com.jingyunbank.etrade.TestCaseBase;
 
 public class ProvinceControllerTest extends TestCaseBase {
 
-	/**
-	 * 测试新增
-	 * @throws Exception
-	 */
-	@Test
-	public void test0() throws Exception{
-		getMockMvc().perform(
-				 put("/api/province/")
-				 .sessionAttr(ServletBox.LOGIN_ID, "1")
-				.param("provinceName", "name")
-				.param("countryID", "1")
-					.contentType(MediaType.APPLICATION_JSON)
-					.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.parseMediaType("application/json;charset=UTF-8")))
-			.andExpect(jsonPath("$.code").value("200"))
-			.andDo(MockMvcResultHandlers.print())
-			.andDo(print());
-		//System.out.println(restTemplate.getForEntity("http://localhost:8080/user", String.class).getBody());
-		
-	}
 	
-	/**
-	 * 删除
-	 * @throws Exception
-	 * 2015年11月18日 qxs
-	 */
-	@Test
-	public void testDel() throws Exception{
-		getMockMvc().perform(
-				 delete("/api/province/35")
-				 .sessionAttr(ServletBox.LOGIN_ID, "1")
-					.contentType(MediaType.APPLICATION_JSON)
-					.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.parseMediaType("application/json;charset=UTF-8")))
-			.andExpect(jsonPath("$.code").value("200"))
-			.andDo(MockMvcResultHandlers.print())
-			.andDo(print());
-		//System.out.println(restTemplate.getForEntity("http://localhost:8080/user", String.class).getBody());
-		
-	}
-	
-	@Test
-	public void testUpdate() throws Exception{
-		getMockMvc().perform(
-				 post("/api/province/1")
-				 .param("provinceName", "北京市")
-				 .sessionAttr(ServletBox.LOGIN_ID, "1")
-					.contentType(MediaType.APPLICATION_JSON)
-					.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.parseMediaType("application/json;charset=UTF-8")))
-			.andExpect(jsonPath("$.code").value("200"))
-			.andDo(MockMvcResultHandlers.print())
-			.andDo(print());
-		//System.out.println(restTemplate.getForEntity("http://localhost:8080/user", String.class).getBody());
-		
-	}
 	/**
 	 * 详情
 	 * @throws Exception
