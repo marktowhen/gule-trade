feff7f9eb6c5886e3468f66610c48c12d22e6703
alex <myfacebookhello@163.com>
2016-04-27 10:53:06 +0800

    update code  about weixin

--- src/main/java/com/jingyunbank/etrade/weixinMessage/dao/MsgBaseDao.java
+++ src/main/java/com/jingyunbank/etrade/weixinMessage/dao/MsgBaseDao.java
@@ -11,24 +11,6 @@ import com.jingyunbank.etrade.api.weixinMessage.bo.MsgText;
 
 public interface MsgBaseDao {
 
-	public MsgBase getById(String id);
 
-	public List<MsgBase> listForPage(MsgBase searchEntity);
-
-	public List<MsgNews> listMsgNewsByBaseId(String[] ids);
-	
-	public MsgText getMsgTextByBaseId(String id);
-	
-	public MsgText getMsgTextBySubscribe();
-	
-	public MsgText getMsgTextByInputCode(String inputcode);
-	
-	public void add(MsgBase entity);
-
-	public void update(MsgBase entity);
-	
-	public void updateInputcode(MsgBase entity);
-
-	public void delete(MsgBase entity);
 
 }
